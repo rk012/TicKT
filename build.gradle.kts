15d84e4a68a4a27ae8b9885f2661c7d7a0ccd0dd
Rishi Kumar <rishi.kumar.2837@gmail.com>
2023-12-24 16:27:55 -0500

    Initial Commit
    If jitpack actually works first time thats pretty crazy

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
+    }
+}
+
+allprojects {
+    group = "io.github.rk012"
+    version = "1.0-SNAPSHOT"
+}
+
+subprojects {
+    repositories {
+        mavenCentral()
+    }
+
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "maven-publish")
+
+    val testImplementation by configurations
+
+    dependencies {
+        testImplementation(kotlin("test"))
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    configure<PublishingExtension> {
+        publications {
+            create<MavenPublication>("maven") {
+                from(components["java"])
+            }
+        }
+    }
+}
